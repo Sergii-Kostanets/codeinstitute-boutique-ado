b48c334488935a78c17e9539b9a6a993a82a99fe
Sergii Kostanets <sergiikostanets@gmail.com>
2024-03-27 20:10:34 +0000

    set AWS_STORAGE_BUCKET_NAME to env var

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -185,7 +185,7 @@ if 'USE_AWS' in os.environ:
     }
 
     # Bucket Configuration
-    AWS_STORAGE_BUCKET_NAME = 'boutique-ado-codeinstitute'
+    AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
     AWS_S3_REGION_NAME = 'eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
